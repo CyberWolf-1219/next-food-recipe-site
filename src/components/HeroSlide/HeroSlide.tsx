fb9603e164c4b1b0a61c9119b9b187984d0a5a1f
BlackMonkey-1219 <bl4ckm0nk3yb4n4n4@gmail.com>
2023-02-20 04:45:52 +0530

    Hero section completed

---
+++ src/components/HeroSlide/HeroSlide.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import Image from 'next/image';
+
+import styleClasses from './styles.module.css';
+
+interface iHeroSlide {
+  position: string;
+  heading: string;
+}
+
+function HeroSlide(props: iHeroSlide) {
+  return (
+    <div
+      data-position={props.position}
+      className={`${styleClasses.slide} absolute aspect-[1/1] w-screen h-auto transition-transform transform-gpu duration-[500ms] bg-white`}
+    >
+      <Image
+        src={''}
+        alt={''}
+        fill={true}
+        className={`aspect-[1/1] w-full h-auto`}
+      />
+      <h2 className={`absolute z-[2] w-fit h-fit left-4 bottom-4`}>
+        {props.heading}
+      </h2>
+    </div>
+  );
+}
+
+export default HeroSlide;
---
+++ src/components/HomeHeroSection/HomeHeroSection.tsx
@@ -0,0 +1,65 @@
+import Image from 'next/image';
+import React, { useState } from 'react';
+import { FaArrowLeft, FaArrowRight } from 'react-icons/fa';
+import IconButton from '../IconButton/IconButton';
+import HeroSlide from '../HeroSlide/HeroSlide';
+
+type slidePositions = [number, number, number, number, number];
+
+function decreaseSlidePositions(positions: slidePositions): slidePositions {
+  const newPositionsArray = positions.map((val) => {
+    return val == 1 ? 5 : val - 1;
+  });
+  console.log(positions, newPositionsArray);
+  return newPositionsArray as slidePositions;
+}
+
+function HomeHeroSection() {
+  const [slidePositions, setSlidePositions] = useState<slidePositions>([
+    1, 2, 3, 4, 5,
+  ]);
+
+  function nextButtonHandler(e: React.MouseEvent<HTMLButtonElement>) {
+    e.preventDefault();
+    setSlidePositions(decreaseSlidePositions);
+  }
+
+  return (
+    <section className={`relative w-full h-fit border-2 border-red-500`}>
+      {/* SLIDE CONTAINER */}
+      <div
+        className={`relative aspect-[1/1] w-screen h-auto flex flex-row bg-white`}
+      >
+        {/* SLIDE */}
+        <HeroSlide
+          heading={'01'}
+          position={slidePositions[0].toString()}
+        />
+        <HeroSlide
+          heading={'02'}
+          position={slidePositions[1].toString()}
+        />
+        <HeroSlide
+          heading={'03'}
+          position={slidePositions[2].toString()}
+        />
+        <HeroSlide
+          heading={'04'}
+          position={slidePositions[3].toString()}
+        />
+        <HeroSlide
+          heading={'05'}
+          position={slidePositions[4].toString()}
+        />
+      </div>
+      {/* CONTROLS */}
+      <div className={'absolute z-[5]  bottom-0 right-0'}>
+        <IconButton action={nextButtonHandler}>
+          <FaArrowRight size={'2rem'} />
+        </IconButton>
+      </div>
+    </section>
+  );
+}
+
+export default HomeHeroSection;
