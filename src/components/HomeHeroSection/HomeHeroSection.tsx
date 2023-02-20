362b54bfc82cc0887793bd2cfec935285fc0bd80
BlackMonkey-1219 <bl4ckm0nk3yb4n4n4@gmail.com>
2023-02-20 11:24:00 +0530

    Separated Logic Into A Different File

--- src/components/HomeHeroSection/HomeHeroSection.tsx
+++ src/components/HomeHeroSection/HomeHeroSection.tsx
@@ -1,28 +1,26 @@
 import Image from 'next/image';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { FaArrowLeft, FaArrowRight } from 'react-icons/fa';
 import IconButton from '../IconButton/IconButton';
 import HeroSlide from '../HeroSlide/HeroSlide';
+import { nextButtonHandler } from './logic';
 
 type slidePositions = [number, number, number, number, number];
-
-function decreaseSlidePositions(positions: slidePositions): slidePositions {
-  const newPositionsArray = positions.map((val) => {
-    return val == 1 ? 5 : val - 1;
-  });
-  console.log(positions, newPositionsArray);
-  return newPositionsArray as slidePositions;
-}
 
 function HomeHeroSection() {
   const [slidePositions, setSlidePositions] = useState<slidePositions>([
     1, 2, 3, 4, 5,
   ]);
 
-  function nextButtonHandler(e: React.MouseEvent<HTMLButtonElement>) {
-    e.preventDefault();
-    setSlidePositions(decreaseSlidePositions);
-  }
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      nextButtonHandler(setSlidePositions);
+    }, 5000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
 
   return (
     <section className={`relative w-full h-fit`}>
@@ -54,7 +52,11 @@ function HomeHeroSection() {
       </div>
       {/* CONTROLS */}
       <div className={'absolute z-[5]  bottom-0 right-0'}>
-        <IconButton action={nextButtonHandler}>
+        <IconButton
+          action={(e) => {
+            nextButtonHandler(setSlidePositions, e);
+          }}
+        >
           <FaArrowRight size={'2rem'} />
         </IconButton>
       </div>
---
+++ src/components/HomeHeroSection/logic.ts
@@ -0,0 +1,19 @@
+type slidePositions = [number, number, number, number, number];
+
+export function decreaseSlidePositions(
+  positions: slidePositions
+): slidePositions {
+  const newPositionsArray = positions.map((val) => {
+    return val == 1 ? 5 : val - 1;
+  });
+  console.log(positions, newPositionsArray);
+  return newPositionsArray as slidePositions;
+}
+
+export function nextButtonHandler(
+  dispatcher: React.Dispatch<React.SetStateAction<slidePositions>>,
+  e?: React.MouseEvent<HTMLButtonElement>
+) {
+  e?.preventDefault();
+  dispatcher(decreaseSlidePositions);
+}
