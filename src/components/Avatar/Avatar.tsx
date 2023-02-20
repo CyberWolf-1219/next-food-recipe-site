2be732f31470d35b981174b48103f4a74d3f8552
BlackMonkey-1219 <bl4ckm0nk3yb4n4n4@gmail.com>
2023-02-20 06:27:40 +0530

    Created Avatar component

---
+++ src/components/Avatar/Avatar.tsx
@@ -0,0 +1,32 @@
+import Image from 'next/image';
+import React from 'react';
+
+const AvatarSizeOptions = {
+  sm: 'w-[2.5rem]',
+  md: 'w-[5rem]',
+  lg: 'w-[7.5rem]',
+};
+
+interface iAvatar {
+  image: string;
+  size: keyof typeof AvatarSizeOptions;
+}
+
+function Avatar(props: iAvatar) {
+  return (
+    <div
+      className={`relative aspect-[1/1] ${
+        AvatarSizeOptions[props.size]
+      } h-auto rounded-full border-2`}
+    >
+      <Image
+        src={props.image}
+        alt={'user avatar'}
+        fill={true}
+        className={'rounded-full object-center'}
+      />
+    </div>
+  );
+}
+
+export default Avatar;
