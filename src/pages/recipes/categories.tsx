91c6aeb9ba7a3d162087ce877aa30646ac090c88
BlackMonkey-1219 <bl4ckm0nk3yb4n4n4@gmail.com>
2023-02-20 16:37:28 +0530

    Created categories page

---
+++ src/pages/recipes/categories.tsx
@@ -0,0 +1,43 @@
+import AllCategoriesSection from '@/components/AllCategoriesSection/AllCategoriesSection';
+import Footer from '@/components/Footer/Footer';
+import Navigation from '@/components/Navigation/Navigation';
+import Head from 'next/head';
+import React from 'react';
+
+function categories() {
+  return (
+    <>
+      <Head>
+        <meta
+          name='description'
+          content='All the of the food categories you can choose from.'
+        />
+        <meta
+          http-equiv='X-UA-Compatible'
+          content='ie=edge'
+        />
+        <meta
+          http-equiv='X-UA-Compatible'
+          content='IE=7'
+        />
+        <meta
+          http-equiv='Content-Type'
+          content='text/html;charset=UTF-8'
+        />
+        <meta
+          name='viewport'
+          content='width=device-width, initial-scale=1.0'
+        />
+      </Head>
+      <header>
+        <Navigation />
+      </header>
+      <main>
+        <AllCategoriesSection />
+      </main>
+      <Footer />
+    </>
+  );
+}
+
+export default categories;
