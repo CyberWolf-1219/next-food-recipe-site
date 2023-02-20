f8d2897baffbfd96d09ab7e00155623b4706eacc
BlackMonkey-1219 <bl4ckm0nk3yb4n4n4@gmail.com>
2023-02-20 09:49:19 +0530

    Created OptInForm Component

---
+++ src/components/OptInForm/OptInForm.tsx
@@ -0,0 +1,59 @@
+import Link from 'next/link';
+import React from 'react';
+import { FaMailBulk } from 'react-icons/fa';
+
+function OptInForm() {
+  return (
+    <form
+      action=''
+      method={'POST'}
+      className={'w-full h-fit py-[2rem] bg-accent/50'}
+    >
+      <h2 className={'text-center'}>Deliciousness to your inbox.</h2>
+      <p className={'text-center'}>
+        Enjoy weekly handpicked recipes and recommendations.
+      </p>
+      <div
+        className={
+          'w-[80%] h-fit mx-auto p-2 flex flex-row items-center justify-center gap-2 bg-white rounded-md'
+        }
+      >
+        <span className={'aspect-[1/1] w-[2rem] h-auto'}>
+          <FaMailBulk
+            size={'2rem'}
+            color={'var(--clr-accent)'}
+          />
+        </span>
+        <input
+          type='email'
+          name='user_email'
+          id='user_email_input'
+          placeholder={'Adde your email here...'}
+          className={
+            'inline-block w-[80%] h-fit mx-auto p-0 font-base text-lg bg-transparent'
+          }
+        />
+      </div>
+      <div className={'w-[80%] h-fit mx-auto mt-4'}>
+        <button
+          className={
+            'w-full h-fit px-[2em] py-[0.5em] mx-auto bg-accent font-semibold'
+          }
+        >
+          JOIN
+        </button>
+      </div>
+      <small className={'block max-w-[40ch] mx-auto mt-2 text-center'}>
+        By joining our newsletter you are aggreeing to our{' '}
+        <Link
+          href={'#'}
+          className={'underline'}
+        >
+          Terms & Services
+        </Link>
+      </small>
+    </form>
+  );
+}
+
+export default OptInForm;
