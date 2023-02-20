340f5f13c5b73f15fddaa963acbf1efe03986f80
BlackMonkey-1219 <bl4ckm0nk3yb4n4n4@gmail.com>
2023-02-20 09:49:48 +0530

    Created HomeOptInSection Component

---
+++ src/components/HomeOptInSection/HomeOptInSection.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Container from '../Container/Container';
+import OptInForm from '../OptInForm/OptInForm';
+
+function HomeOptInSection() {
+  return (
+    <section className={'w-full h-fit px-4 pt-8'}>
+      <Container>
+        <OptInForm />
+      </Container>
+    </section>
+  );
+}
+
+export default HomeOptInSection;
