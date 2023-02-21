b8c015e50b1067e684b6439a6fcf88d37c5ea54e
BlackMonkey-1219 <bl4ckm0nk3yb4n4n4@gmail.com>
2023-02-21 07:11:10 +0530

    Added Width & Height Option Props

--- src/components/IconButton/IconButton.tsx
+++ src/components/IconButton/IconButton.tsx
@@ -1,7 +1,19 @@
 import React, { ReactNode } from 'react';
 
+const widthOptions = {
+  full: 'w-full',
+  fit: 'w-fit',
+};
+
+const heightOptions = {
+  full: 'w-full',
+  fit: 'w-fit',
+};
+
 interface iIconButton {
   children: ReactNode | ReactNode[];
+  width?: keyof typeof widthOptions;
+  height?: keyof typeof heightOptions;
   action?: (e: React.MouseEvent<HTMLButtonElement>) => void;
   classes?: string;
 }
@@ -10,7 +22,9 @@ function IconButton(props: iIconButton) {
   return (
     <button
       onClick={props.action}
-      className={`inline-block w-full h-full p-2 ${props.classes ?? null}`}
+      className={`inline-block ${widthOptions[props.width ?? 'full']} ${
+        heightOptions[props.height ?? 'fit']
+      } p-2 ${props.classes ?? null}`}
     >
       {props.children}
     </button>
