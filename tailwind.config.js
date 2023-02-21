7fc0f361729c4ddf94c7d577df17de29bc12a43b
BlackMonkey-1219 <bl4ckm0nk3yb4n4n4@gmail.com>
2023-02-21 10:02:57 +0530

    Changed default shadows

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
- 
+    './app/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+
     // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx}",
+    './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
+    boxShadow: {
+      sm: '0px 1px 5px -1px rgba(0,0,0,1)',
+      md: '0px 5px 8px 0px rgba(0,0,0,1)',
+      lg: '0px 8px 25px 0px rgba(0,0,0,1)',
+    },
     extend: {
-      colors:{
-        'primary':'hsl(0, 0%, 98%)',
-        'secondary':'hsl(0, 0%, 0%)',
-        'accent':'hsl(15, 100%, 59%)'
-      }
+      colors: {
+        primary: 'hsl(0, 0%, 98%)',
+        secondary: 'hsl(0, 0%, 0%)',
+        accent: 'hsl(15, 100%, 59%)',
+      },
     },
   },
   plugins: [],
-}
+};
