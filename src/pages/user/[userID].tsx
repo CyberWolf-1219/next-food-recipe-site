22efc1efef64c19d50d9d1d1403175ab85b60c3b
BlackMonkey-1219 <bl4ckm0nk3yb4n4n4@gmail.com>
2023-02-22 11:53:14 +0530

    Create Favourites Page

---
+++ src/pages/user/[userID].tsx
@@ -0,0 +1,123 @@
+import Container from '@/components/Container/Container';
+import Footer from '@/components/Footer/Footer';
+import IconButton from '@/components/IconButton/IconButton';
+import Navigation from '@/components/Navigation/Navigation';
+import RecipeCard from '@/components/RecipeCard/RecipeCard';
+import Head from 'next/head';
+import React from 'react';
+
+import { AiFillHeart } from 'react-icons/ai';
+import { MdDelete } from 'react-icons/md';
+
+function Favorites() {
+  return (
+    <>
+      <Head>
+        <meta
+          http-equiv='X-UA-Compatible'
+          content='IE=7'
+        />
+        <meta
+          name='description'
+          content=''
+        />
+        <meta
+          http-equiv='X-UA-Compatible'
+          content='ie=edge'
+        />
+        <meta
+          http-equiv='Content-Type'
+          content='text/html;charset=UTF-8'
+        />
+        <meta
+          name='viewport'
+          content='width=device-width, initial-scale=1.0'
+        />
+      </Head>
+      <header>
+        <Navigation />
+      </header>
+      <main>
+        <section className={'w-full h-fit px-4'}>
+          <Container>
+            <div
+              className={
+                'w-full h-fit mt-8 flex flex-col lg:flex-row items-start lg:items-center justify-between'
+              }>
+              <h1
+                className={
+                  'mt-0 mb-0 flex flex-row items-center justify-start gap-[0.25em]'
+                }>
+                <span
+                  className={'relative inline-block w-fit h-fit text-inherit'}>
+                  <AiFillHeart className={'text-inherit fill-accent'} />
+                  <AiFillHeart
+                    size={'1.15em'}
+                    className={
+                      'absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]  fill-accent/50 blur-[2px]'
+                    }
+                  />
+                </span>
+                Favourites
+              </h1>
+              <select
+                name='sort_by'
+                id='sort_type_input'
+                defaultValue={'0'}
+                className={
+                  'w-full lg:w-fit h-fit mt-4 px-3 py-2 border-2 rounded-md'
+                }>
+                <option
+                  value='0'
+                  disabled={true}>
+                  Sort
+                </option>
+                <option value='1'>Likes</option>
+                <option value='2'>Comments</option>
+                <option value='3'>Created Date</option>
+                <option value='4'>Ingredient Count</option>
+                <option value='5'>Prepare Time</option>
+              </select>
+            </div>
+            <hr />
+          </Container>
+        </section>
+        <section className={'w-full h-fit px-4 py-8'}>
+          <Container>
+            <IconButton
+              classes={
+                'flex flex-row items-center justify-center gap-2 bg-accent text-white shadow-md shadow-accent/30'
+              }
+              disabled={true}>
+              <MdDelete />
+              Delete Selected
+            </IconButton>
+            <ul
+              className={
+                'w-full h-fit mt-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 auto-rows-auto gap-4'
+              }>
+              {[...Array(20)].map((_) => {
+                return (
+                  <li key={`favouirte_recipe_${Math.random()}`}>
+                    <RecipeCard
+                      image={''}
+                      name={'Fav Recipe ###'}
+                      likes={890000}
+                      comments={500000}
+                      createdDate={Date.now()}
+                      authorImage={''}
+                      authorName={''}
+                    />
+                  </li>
+                );
+              })}
+            </ul>
+          </Container>
+        </section>
+      </main>
+      <Footer />
+    </>
+  );
+}
+
+export default Favorites;
