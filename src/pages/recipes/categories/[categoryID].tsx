8c3196311b59029edb6a9638181ab0b7ce05a36f
BlackMonkey-1219 <bl4ckm0nk3yb4n4n4@gmail.com>
2023-02-22 07:13:14 +0530

    Added Categories Route, Index, CategoryPage

---
+++ src/pages/recipes/categories/[categoryID].tsx
@@ -0,0 +1,112 @@
+import Container from '@/components/Container/Container';
+import Footer from '@/components/Footer/Footer';
+import Navigation from '@/components/Navigation/Navigation';
+import RecipeCard from '@/components/RecipeCard/RecipeCard';
+import Head from 'next/head';
+import Image from 'next/image';
+import React from 'react';
+
+function Category() {
+  return (
+    <>
+      <Head>
+        <meta
+          http-equiv='X-UA-Compatible'
+          content='IE=7'
+        />
+        <meta
+          name='description'
+          content=''
+        />
+        <meta
+          http-equiv='X-UA-Compatible'
+          content='ie=edge'
+        />
+        <meta
+          http-equiv='Content-Type'
+          content='text/html;charset=UTF-8'
+        />
+        <meta
+          name='viewport'
+          content='width=device-width, initial-scale=1.0'
+        />
+      </Head>
+      <header>
+        <Navigation />
+      </header>
+      <main>
+        <Image
+          src={''}
+          alt={''}
+          className={'aspect-[16/3] w-full h-auto object-cover border-2'}
+        />
+        <section className={'w-full h-fit min-h-screen px-4'}>
+          <Container>
+            <div
+              className={
+                'w-full h-fit flex flex-row items-center justify-start gap-4'
+              }>
+              <h1>Category Name</h1>
+              <span className={'inline-block w-max whitespace-nowrap'}>
+                (### Recipes)
+              </span>
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum a
+              ullam labore perspiciatis! Quidem, quae doloribus sequi quis
+              consectetur neque repudiandae. Adipisci, molestias dicta est illo
+              facilis delectus quas nobis nulla reiciendis, qui minima quae
+              natus odit numquam! Inventore earum quidem commodi voluptas amet
+              mollitia quae ad placeat corrupti ducimus.
+            </p>
+            <div
+              className={
+                'w-full h-fit mb-8 flex flex-row items-center justify-end gap-3 font-semibold text-lg'
+              }>
+              <label htmlFor='sort_type_input'>Sort By: </label>
+              <select
+                name='sort_by'
+                id='sort_type_input'
+                defaultValue={'0'}
+                className={'px-3 py-2 border-2 rounded-md'}>
+                <option
+                  value='0'
+                  disabled={true}>
+                  Sort
+                </option>
+                <option value='1'>Likes</option>
+                <option value='2'>Comments</option>
+                <option value='3'>Created Date</option>
+                <option value='4'>Ingredient Count</option>
+                <option value='5'>Prepare Time</option>
+              </select>
+            </div>
+            <ul
+              className={
+                'w-full h-fit grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 auto-rows-auto gap-4 '
+              }>
+              {[...Array(20)].map((_) => {
+                return (
+                  <li key={`recipe_${Math.random()}`}>
+                    <RecipeCard
+                      image={''}
+                      name={'Recipe ###'}
+                      likes={5000}
+                      comments={1200}
+                      createdDate={Date.now()}
+                      authorImage={''}
+                      authorName={'Uncle Po'}
+                    />
+                  </li>
+                );
+              })}
+            </ul>
+          </Container>
+        </section>
+      </main>
+      <Footer />
+    </>
+  );
+}
+
+export default Category;
---
+++ src/pages/recipes/categories/index.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+import Head from 'next/head';
+import Navigation from '@/components/Navigation/Navigation';
+import Footer from '@/components/Footer/Footer';
+import AllCategoriesSection from '@/components/AllCategoriesSection/AllCategoriesSection';
+
+function Categories() {
+  return (
+    <>
+      <Head>
+        <meta
+          name='description'
+          content='All the of the food categories you can choose from.'
+        />
+        <meta
+          http-equiv='X-UA-Compatible'
+          content='ie=edge'
+        />
+        <meta
+          http-equiv='X-UA-Compatible'
+          content='IE=7'
+        />
+        <meta
+          http-equiv='Content-Type'
+          content='text/html;charset=UTF-8'
+        />
+        <meta
+          name='viewport'
+          content='width=device-width, initial-scale=1.0'
+        />
+      </Head>
+      <header>
+        <Navigation />
+      </header>
+      <main>
+        <AllCategoriesSection />
+      </main>
+      <Footer />
+    </>
+  );
+}
+
+export default Categories;
