1deea719f3eae4a16d075c1837aa99b58dec146e
BlackMonkey-1219 <bl4ckm0nk3yb4n4n4@gmail.com>
2023-02-23 10:14:50 +0530

    Created Recipe View Page Comment Write Section

---
+++ src/components/RecipeViewWriteComment/RecipeViewWriteComment.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import Container from '../Container/Container';
+import IconButton from '../IconButton/IconButton';
+
+import { RiSendPlaneFill } from 'react-icons/ri';
+
+function RecipeViewWriteComment() {
+  return (
+    <section
+      className={
+        'md:col-start-1 md:col-end-7 lg:col-end-13 w-full h-fit mb-8 px-4'
+      }>
+      <Container>
+        <h2>Write a comment</h2>
+        <form
+          action=''
+          className={'w-full h-fit p-4 bg-gray-200'}>
+          <textarea
+            name=''
+            id=''
+            cols={30}
+            rows={10}
+            className={'w-full h-fit p-2 rounded-md'}></textarea>
+          <IconButton
+            classes={
+              'flex flex-row items-center justify-center gap-2 bg-accent shadow-sm shadow-accent/30 text-white'
+            }>
+            <RiSendPlaneFill />
+            Post Comment
+          </IconButton>
+        </form>
+      </Container>
+    </section>
+  );
+}
+
+export default RecipeViewWriteComment;
