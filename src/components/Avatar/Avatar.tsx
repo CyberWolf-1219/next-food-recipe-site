d00f6fcc541da4866b2e82cc2da6678b2ae0f63f
BlackMonkey-1219 <bl4ckm0nk3yb4n4n4@gmail.com>
2023-02-23 10:07:33 +0530

    Changed Sizes

--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -2,15 +2,16 @@ import Image from 'next/image';
 import React from 'react';
 
 const AvatarSizeOptions = {
-  sm: 'w-[2rem]',
-  md: 'w-[5rem]',
-  lg: 'w-[6.5rem]',
+  sm: 'w-[1.5rem]',
+  md: 'w-[3rem]',
+  lg: 'w-[5rem]',
   xl: 'w-[8rem]',
 };
 
 interface iAvatar {
   image: string;
   size: keyof typeof AvatarSizeOptions;
+  classes?: string;
 }
 
 function Avatar(props: iAvatar) {
@@ -18,7 +19,7 @@ function Avatar(props: iAvatar) {
     <div
       className={`relative aspect-[1/1] ${
         AvatarSizeOptions[props.size]
-      } h-auto rounded-full border-2`}>
+      } h-auto rounded-full border-2 ${props.classes ?? null}`}>
       <Image
         src={props.image}
         alt={'user avatar'}
