33b95b13093a27c08ca2a9f4c4fdd9208c01a7ca
BlackMonkey-1219 <bl4ckm0nk3yb4n4n4@gmail.com>
2023-02-23 10:15:33 +0530

    Created Recipe View Page Optin Section

---
+++ src/components/RecipeViewOptinForm/RecipeViewOptinForm.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import OptInForm from '../OptInForm/OptInForm';
+
+function RecipeViewOptinForm() {
+  return (
+    <section
+      className={'md:col-start-1 md:col-end-7 lg:col-end-13 bg-accent/20'}>
+      <OptInForm />
+    </section>
+  );
+}
+
+export default RecipeViewOptinForm;
