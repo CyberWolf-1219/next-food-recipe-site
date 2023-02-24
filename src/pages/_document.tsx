d5b4fcc05ec8e18ca3134742b60e75e670d55124
BlackMonkey-1219 <bl4ckm0nk3yb4n4n4@gmail.com>
2023-02-24 06:00:34 +0530

    Added Modal Portal

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,14 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang='en'>
       <Head />
       <body>
+        <div id={'modal_mount_point'}></div>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
