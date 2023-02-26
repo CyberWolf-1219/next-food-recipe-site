bd192e3dafe21de3d718d9b42e5a31122bead7bf
BlackMonkey-1219 <bl4ckm0nk3yb4n4n4@gmail.com>
2023-02-26 11:20:45 +0530

    Slide Now Uses Fetched Image

--- src/components/HeroSlide/HeroSlide.tsx
+++ src/components/HeroSlide/HeroSlide.tsx
@@ -4,24 +4,24 @@ import Image from 'next/image';
 import styleClasses from './styles.module.css';
 
 interface iHeroSlide {
+  image: string;
   position: string;
   heading: string;
 }
 
-function HeroSlide(props: iHeroSlide) {
+function HeroSlide({ image, position, heading }: iHeroSlide) {
   return (
     <div
-      data-position={props.position}
-      className={`${styleClasses.slide} absolute aspect-[1/1] lg:aspect-[16/9] w-screen lg:w-full h-auto transition-transform transform-gpu duration-[500ms] bg-white`}
-    >
+      data-position={position}
+      className={`${styleClasses.slide} absolute aspect-[1/1] lg:aspect-[16/9] w-screen lg:w-full h-auto transition-transform transform-gpu duration-[500ms] bg-white`}>
       <Image
-        src={''}
-        alt={''}
+        src={image}
+        alt={heading}
         fill={true}
         className={`aspect-[1/1] lg:aspect-[16/9] w-full h-auto`}
       />
       <h2 className={`absolute z-[2] w-fit h-fit left-4 bottom-4`}>
-        {props.heading}
+        {heading}
       </h2>
     </div>
   );
