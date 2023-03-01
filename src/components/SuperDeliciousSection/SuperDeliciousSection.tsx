ff4aeb7711f66a05d520d05971a6d3a662926450
BlackMonkey-1219 <bl4ckm0nk3yb4n4n4@gmail.com>
2023-03-01 12:38:30 +0530

    Connected to popular recipes api endpoint

--- src/components/SuperDeliciousSection/SuperDeliciousSection.tsx
+++ src/components/SuperDeliciousSection/SuperDeliciousSection.tsx
@@ -1,67 +1,14 @@
-import React, { useState } from 'react';
+import React, { useEffect } from 'react';
 import Container from '../Container/Container';
 import RecipeCard from '../RecipeCard/RecipeCard';
-
-type SuperDeliciousRecipy = {
-  image: string;
-  name: string;
-  likes: number;
-  comments: number;
-  createdDate: number;
-  authorName: string;
-  authorImage: string;
-};
+import useFetch from '@/hooks/useFetch';
 
 function SuperDeliciousSection() {
-  const [trendingRecipes, setTrendingRecipes] = useState<
-    Array<SuperDeliciousRecipy>
-  >([
-    {
-      image: '',
-      name: 'Chocolate Cake',
-      likes: 1400,
-      comments: 987,
-      createdDate: Date.now(),
-      authorName: 'Kaely Brimston',
-      authorImage: '',
-    },
-    {
-      image: '',
-      name: 'Chocolate Cake',
-      likes: 1400,
-      comments: 987,
-      createdDate: Date.now(),
-      authorName: 'Kaely Brimston',
-      authorImage: '',
-    },
-    {
-      image: '',
-      name: 'Chocolate Cake',
-      likes: 1400,
-      comments: 987,
-      createdDate: Date.now(),
-      authorName: 'Kaely Brimston',
-      authorImage: '',
-    },
-    {
-      image: '',
-      name: 'Chocolate Cake',
-      likes: 1400,
-      comments: 987,
-      createdDate: Date.now(),
-      authorName: 'Kaely Brimston',
-      authorImage: '',
-    },
-    {
-      image: '',
-      name: 'Chocolate Cake',
-      likes: 1400,
-      comments: 987,
-      createdDate: Date.now(),
-      authorName: 'Kaely Brimston',
-      authorImage: '',
-    },
-  ]);
+  const [execute, result] = useFetch();
+
+  useEffect(() => {
+    execute('/api/recipes/popular_recipes', {});
+  }, [execute]);
 
   return (
     <section className={'px-4 lg:pb-[8rem]'}>
@@ -73,17 +20,17 @@ function SuperDeliciousSection() {
           className={
             'w-full h-fit grid gird-cols-1 md:grid-cols-3 lg:grid-cols-3 auto-rows-fr gap-4'
           }>
-          {trendingRecipes.map((recipeObj) => {
+          {(result as { recipes: Array<Recipe> })?.recipes?.map((recipeObj) => {
             return (
               <li key={`trending_recipe_${Math.random()}`}>
                 <RecipeCard
-                  image={recipeObj.image}
-                  name={recipeObj.name}
-                  likes={recipeObj.likes}
-                  comments={recipeObj.comments}
-                  createdDate={recipeObj.createdDate}
-                  authorImage={recipeObj.image}
-                  authorName={recipeObj.authorName}
+                  image={recipeObj.strMealThumb}
+                  name={recipeObj.strMeal}
+                  likes={500000}
+                  comments={80000}
+                  createdDate={Date.now()}
+                  authorImage={''}
+                  authorName={recipeObj.strSource}
                 />
               </li>
             );
