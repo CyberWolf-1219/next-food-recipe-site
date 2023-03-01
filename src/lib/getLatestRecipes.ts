7d001d85f5d7c94066688d0134b76fedc2ae8f45
BlackMonkey-1219 <bl4ckm0nk3yb4n4n4@gmail.com>
2023-03-01 12:32:13 +0530

    Created Latest Recipe Endpoint Logic

---
+++ src/lib/getLatestRecipes.ts
@@ -0,0 +1,18 @@
+const ENDPOINT = 'https://www.themealdb.com/api/json/v1/1/random.php';
+
+export default async function getLatestRecipes() {
+  const latestRecipes: Array<Recipe> = [];
+
+  try {
+    for (let i = 0; i < 8; i++) {
+      const response = await fetch(ENDPOINT);
+      const jsonResult: RandomRecipeResponse = await response.json();
+      latestRecipes.push(jsonResult.meals[0]);
+    }
+
+    return latestRecipes;
+  } catch (error) {
+    console.log(error);
+    return [];
+  }
+}
