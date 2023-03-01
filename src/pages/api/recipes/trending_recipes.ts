1ee87d90f2333f903927614efe03ae3fc9f06cbc
BlackMonkey-1219 <bl4ckm0nk3yb4n4n4@gmail.com>
2023-03-01 12:29:35 +0530

    Renamed

--- src/pages/api/recipes/trending_recipes.ts
+++ src/pages/api/recipes/trending_recipes.ts
@@ -4,7 +4,6 @@ import { NextApiRequest, NextApiResponse } from 'next';
 const ENDPOINTS = {
   search: 'www.themealdb.com/api/json/v1/1/search.php?s=',
   details: 'www.themealdb.com/api/json/v1/1/lookup.php?i=',
-  categories: 'www.themealdb.com/api/json/v1/1/categories.php',
   mainIngredient: 'www.themealdb.com/api/json/v1/1/filter.php?i=',
   filterByCategory: 'www.themealdb.com/api/json/v1/1/filter.php?i=',
 };
