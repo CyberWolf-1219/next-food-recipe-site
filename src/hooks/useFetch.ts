e2566a826ef065fd9624270a2055a9ad32cebad0
BlackMonkey-1219 <110360091+BlackMonkey-1219@users.noreply.github.com>
2023-03-07 07:49:54 +0530

    Refactored uesFetch

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -1,14 +1,12 @@
 import { useState, useRef, useCallback } from 'react';
 
 export default function useFetch() {
-  const [result, setResult] = useState<any>();
-
   const execute = useCallback(async (url: string, options: RequestInit) => {
     console.log('FETCHING...');
     const response = await fetch(url, options);
     const jsonResult = await response.json();
-    setResult(jsonResult);
+    return jsonResult;
   }, []);
 
-  return [execute, result] as const;
+  return execute;
 }
