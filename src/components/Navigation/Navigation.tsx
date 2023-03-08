6030e1176129888c07c6e9b8266ef7ca0fc0b318
BlackMonkey-1219 <110360091+BlackMonkey-1219@users.noreply.github.com>
2023-03-08 07:19:05 +0530

    Changed mobile nav panel

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Link from 'next/link';
+import styleClasses from './style.module.css';
 
 import Container from '../Container/Container';
 import IconButton from '../IconButton/IconButton';
@@ -8,6 +9,7 @@ import Avatar from '../Avatar/Avatar';
 
 import { FaHamburger } from 'react-icons/fa';
 import UserMenu from '../UserMenu/UserMenu';
+import { GiHamburgerMenu } from 'react-icons/gi';
 
 function Navigation() {
   const [navOpen, setNavOpen] = useState(false);
@@ -25,33 +27,17 @@ function Navigation() {
 
   return (
     <nav
-      className={`relative z-[100] w-full h-fit bg-white shadow-sm shadow-secondary/50`}>
+      className={`z-[100] w-full h-fit mb-[5rem] bg-white shadow-sm shadow-secondary/50`}>
       <Container>
         <div
-          className={`lg:relative w-full h-fit lg:flex flex-row items-center justify-between`}>
-          {/* LOGO & NAV OPEN BUTTON */}
-          <div
-            className={`z-[50] w-full h-fit py-2 flex flex-row items-center justify-between md:justify-center lg:justify-start bg-white`}>
-            <Logo
-              classes={`w-full h-auto max-w-[8rem] sm:max-w-[8rem] md:max-w-[12rem] px-2`}
-            />
+          className={`relative w-full h-fit py-4 flex flex-row items-center justify-start md:justify-between`}>
+          {/* LOGO */}
+          <Logo classes={`w-[12rem] h-auto px-2`} />
 
-            <IconButton
-              action={navClickHandler}
-              width={'fit'}
-              height={'fit'}
-              classes={`md:hidden`}>
-              <FaHamburger
-                color={'var(--clr-secondary)'}
-                className={'ascpect-[1/1] w-[2rem] md:w-[4rem] h-auto mx-auto'}
-              />
-            </IconButton>
-          </div>
           {/* NAV LINKS */}
           <ul
-            className={`absolute md:static top-[4rem] bottom-0 left-0 right-0 w-full h-fit p-4 flex flex-col md:flex-row items-start lg:items-center justify-start md:justify-center lg:justify-end gap-8 bg-white bg-transparent font-semibold md:translate-y-0 ${
-              navOpen ? 'translate-y-0' : 'translate-y-[-150%]'
-            } transition-transform duration-500 transform-gpu`}>
+            data-state={`${navOpen}`}
+            className={`${styleClasses.navigation_panel} absolute md:static inset-0 z-[10] w-screen md:w-fit h-screen md:h-fit flex flex-col md:flex-row items-center justify-center gap-[4rem] md:gap-[1rem] bg-accent md:bg-transparent font-semibold text-6xl md:text-lg text-primary md:text-secondary transition-[clip-path] duration-500 font-primary`}>
             <li>
               <Link href={'/'}>Home</Link>
             </li>
@@ -68,21 +54,37 @@ function Navigation() {
               <Link href={'/about'}>About Us</Link>
             </li>
           </ul>
+
           {/* USER AVATAR */}
           <IconButton
             width={'fit'}
             height={'fit'}
-            action={menuOpenHandler}>
+            action={menuOpenHandler}
+            classes={'ml-auto md:ml-[6rem]'}>
             <Avatar
-              size={'md'}
+              size={'lg'}
               image={''}
+              classes={'text-accent'}
             />
           </IconButton>
+
+          {/* MENU BTN */}
+          <IconButton
+            action={navClickHandler}
+            width={'fit'}
+            height={'fit'}
+            classes={`relative z-[100] md:hidden`}>
+            <GiHamburgerMenu
+              color={`${navOpen ? 'var(--clr-primary)' : 'var(--clr-accent)'}`}
+              className={'ascpect-[1/1] w-[3rem] md:w-[4rem] h-auto mx-auto'}
+            />
+          </IconButton>
+
           {/* USER MENU */}
           {menuOpen ? (
             <div
               className={
-                'absolute z-[100] top-[4.5rem] right-8 w-fit min-w-[8rem] h-fit p-4 bg-white rounded-md border-2 font-semibold'
+                'absolute z-[100] top-[4.8rem] right-[1rem] w-fit min-w-[8rem] h-fit p-4 bg-white rounded-md border-2 font-semibold'
               }>
               <UserMenu />
             </div>
