2ad10157288939b1fc0b5f8a23be434fce498a51
BlackMonkey-1219 <110360091+BlackMonkey-1219@users.noreply.github.com>
2023-03-11 06:20:45 +0530

    Re-designed optin form & section

--- src/components/HomeOptInSection/HomeOptInSection.tsx
+++ src/components/HomeOptInSection/HomeOptInSection.tsx
@@ -4,10 +4,8 @@ import OptInForm from '../OptInForm/OptInForm';
 
 function HomeOptInSection() {
   return (
-    <section className={'w-full h-fit px-4 mb-[5rem] bg-accent/10'}>
-      <Container>
-        <OptInForm />
-      </Container>
+    <section className={'w-full h-fit px-4 mb-[5rem]'}>
+      <OptInForm />
     </section>
   );
 }
--- src/components/OptInForm/OptInForm.tsx
+++ src/components/OptInForm/OptInForm.tsx
@@ -2,24 +2,26 @@ import Link from 'next/link';
 import React from 'react';
 import { FaMailBulk } from 'react-icons/fa';
 
+import styleClasses from './style.module.css';
+
 function OptInForm() {
   return (
     <form
       action=''
       method={'POST'}
-      className={'w-full h-fit py-[2rem]'}
-    >
+      className={`${styleClasses.optin_form} aspect-[16/5] w-full h-fit py-[2rem] flex flex-col items-center justify-center`}>
       <h2 className={'w-full max-w-[8em] mx-auto text-center lg:text-7xl'}>
         Deliciousness to your inbox.
       </h2>
-      <p className={'text-center'}>
+
+      <p className={'font-normal text-center'}>
         Enjoy weekly handpicked recipes and recommendations.
       </p>
+
       <div
         className={
-          'w-[80%] lg:w-[50%] h-fit mx-auto p-2 flex flex-row items-center justify-center gap-2 bg-white rounded-md'
-        }
-      >
+          'w-[80%] lg:w-[50%] max-w-[500px] h-fit mx-auto p-2 flex flex-row items-center justify-center gap-2 bg-white rounded-md'
+        }>
         <span className={'aspect-[1/1] w-[2rem] h-auto'}>
           <FaMailBulk
             size={'2rem'}
@@ -36,21 +38,21 @@ function OptInForm() {
           }
         />
       </div>
-      <div className={'w-[80%] lg:w-[50%] h-fit mx-auto mt-4'}>
+
+      <div className={'w-[80%] lg:w-[50%] max-w-[500px] h-fit mx-auto mt-4'}>
         <button
           className={
-            'w-full h-fit px-[2em] py-[0.5em] mx-auto bg-accent font-semibold'
-          }
-        >
+            'w-full h-fit px-[2em] py-[0.5em] mx-auto bg-accent font-semibold text-white'
+          }>
           JOIN
         </button>
       </div>
+
       <small className={'block max-w-[40ch] mx-auto mt-2 text-center'}>
         By joining our newsletter you are aggreeing to our{' '}
         <Link
           href={'#'}
-          className={'underline'}
-        >
+          className={'underline'}>
           Terms & Services
         </Link>
       </small>
